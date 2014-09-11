cf5516f1f86571cfa02345d66c28017cff498f9a
Apoorv Bhargava <apoorvbhargava89@gmail.com>
2014-09-10 22:15:04 -0700

    UDP+TCP+FORK

---
+++ midPlace/client.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define UDP_PORT 7865
+#define DGRAM_SIZE 65500
+#define SPLITS 4
+
+char *splits[SPLITS];
+long int splitLength = 0;
+int numSplits = 0;  		/* number of splits */
+
+/* change this function to include arguments. Naming wrong !
+   The UDP socket should bind to client address, not server address.
+ */
+void startUdp(int pcounter) {
+  int readPtr = 0;
+  struct sockaddr_in cliAddr, serAddr; 
+  socklen_t cliAddrLen;
+  int socketFd, rc;
+  socketFd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socketFd < 0) {
+    fprintf(stderr, "Error: Creating Socket");
+    exit(1);
+  }
+  bzero((char *) &serAddr, sizeof(serAddr));
+  serAddr.sin_family = AF_INET;
+  serAddr.sin_addr.s_addr = INADDR_ANY;
+  serAddr.sin_port = htons(UDP_PORT+pcounter);
+  //printf("port number %d\n", ntohs(serAddr.sin_port));
+  if (bind(socketFd, (struct sockaddr *) &serAddr, sizeof(serAddr)) < 0) {
+    fprintf(stderr, "Error: Binding to Socket");
+    exit(1);
+  }
+  cliAddrLen = sizeof(cliAddr);
+  bzero(splits[pcounter], splitLength);
+  while (readPtr < splitLength) {
+    rc = recvfrom(socketFd, (splits[pcounter] + readPtr), DGRAM_SIZE, 0, 
+          (struct sockaddr *) &cliAddr, &cliAddrLen);
+    if (rc < 0) {
+      fprintf(stderr, "Error: Receiving Data");
+      exit(1);
+    }
+    readPtr += rc;
+    printf("Data Read: %d, Total Data: %d\n", rc, readPtr);
+  }
+
+  close(socketFd);
+  printf("%d\n", pcounter);
+  exit(0);
+}
+
+int main(int argc, char *argv[]) {
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  char buffer[200];
+  char udp_port[6];
+	int icounter = 0 ;
+	
+  if (argc < 3) {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[2]);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) { 
+    fprintf(stderr, "ERROR opening socket");
+    exit(1);
+  }
+  server = gethostbyname(argv[1]);
+  if (server == NULL) {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(1);
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+  serv_addr.sin_port = htons(portno);
+    
+	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "ERROR connecting");
+    exit(1);
+  }
+  bzero(&udp_port, sizeof(udp_port));
+  sprintf(udp_port, "%ld", (long int) UDP_PORT);
+  n = write(sockfd, udp_port,sizeof(udp_port));
+  if (n < 0) { 
+    fprintf(stderr, "ERROR writing to socket");
+    exit(1);
+  }
+
+  bzero(buffer, 200);
+  n = read(sockfd, buffer, 200);
+  if (n < 0) {
+    fprintf(stderr, "ERROR reading from socket");
+    exit(1);
+  }
+	
+	/* check this buffer + 1, buffer + 2. 
+	If you send the message as "4 10000" from server, 
+	then buffer[0] = 4, buffer[1] = space, buffer[2] = 1, buffer[3] = 0, buffer[4] = 0 and 
+	so on, as buffer is a simple character array.
+	*/
+	
+  splitLength = atol((buffer+2));
+	numSplits = atoi(&buffer[0]); // Not used, we will use global SPLITS value
+	//printf("%ld %d\n",splitLength,numSplits);
+	//exit(1);
+	for(icounter=0; icounter < SPLITS;icounter++){
+		splits[icounter] = malloc(splitLength+1);
+	}
+  /* creating the udp */
+  /* this needs to be forked */
+	
+	/* Inserting code for forking processes */
+	int pcounter = 0; /* Counter to spawn new process */
+	pid_t childId = 0;
+	int status = -1;
+	
+	/* Number of processes to be spawned = Number of splits */
+	while(pcounter < numSplits)
+	{
+		childId = fork();
+		if(childId == 0)
+		{
+		  fprintf(stdout, "Child Process-%d started \n",pcounter+1);
+			startUdp(pcounter);
+			fprintf(stdout, "Child Process-%d exiting \n",pcounter+1);
+		}
+		else if(childId < 0){
+			fprintf(stderr, "ERROR: Child Process Creation Failed");
+		}
+		else
+		{
+			fprintf(stdout, "Parent Process continuing \n");
+			pcounter++;
+			/* check where to handle UDP port number increase. Better handle it here and 
+			pass the new UDP port number to child process (similar to server part)
+			*/
+			//udpPort++;    
+		}
+	}
+	
+	/* Parent processes waits for child processes to terminate 
+	Signal Handling left !! 
+	*/
+	while (wait(&status) != -1);
+	
+  //startUdp();
+  close(sockfd);
+  return 0;
+}
---
+++ midPlace/server.c
@@ -0,0 +1,196 @@
+/* Server side module for TCP command connection */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <time.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SPLITS 4
+#define DGRAM_SIZE 65500
+long int splitSize = 0;
+char f_name[50];
+char *splits[SPLITS];
+int udpPort;
+
+void bufferFile() {
+  long int fileSize = 0;
+  int i = 0;
+  size_t size = 0;
+  FILE *stream;
+  stream = fopen(f_name, "rb");
+  fseek(stream, 0L, SEEK_END);
+  fileSize = ftell(stream);
+	printf("fileSize-%ld\n",fileSize);
+  fseek(stream, 0L, SEEK_SET);
+  splitSize = fileSize / SPLITS;
+  for(i = 0; i < SPLITS; i++) {
+    splits[i] = malloc(splitSize+1);
+    size=fread(splits[i],1,splitSize,stream);
+    splits[i][size]=0;
+  }
+  fclose(stream);
+}
+
+void startUdp(int chunk, struct sockaddr_in addr) {
+  int sendPtr = 0;
+  int udpSocket;
+  int sendSize = DGRAM_SIZE;
+  socklen_t cliLen;
+  struct sockaddr_in cliAddr;
+  udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+  if (udpSocket < 0) {
+    fprintf(stderr, "Error: Creating UDP Socket");
+    exit(1);
+  }
+	
+  bzero((char *) &cliAddr, sizeof(cliAddr));
+  cliAddr.sin_family = AF_INET;
+  inet_aton(inet_ntoa(addr.sin_addr), &cliAddr.sin_addr);
+  cliAddr.sin_port = htons(udpPort+chunk);
+  cliLen = sizeof(cliAddr);
+  while (sendPtr <= splitSize) {
+    /* send only the amount left */
+    if ((splitSize - sendPtr) < DGRAM_SIZE) {
+      sendSize = splitSize - sendPtr;
+    } else {
+      sendSize = DGRAM_SIZE;
+    }
+    if(sendto(udpSocket, (splits[chunk] + sendPtr), sendSize, 0, 
+             (struct sockaddr *)&cliAddr, cliLen) < 0) {
+      fprintf(stderr, "Error: Sending Data");
+      exit(1);
+    }
+    sendPtr += DGRAM_SIZE;
+  }
+  close(udpSocket);
+	exit(0);
+}
+
+
+int main(int argc, char *argv[]) {
+  int sockfd, newsockfd, portno;
+  socklen_t clilen;
+  char recv_udp_port[sizeof(int) + 1];
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
+	
+  /* timing */
+  clock_t begin, end;
+  double time_spent;
+  begin = clock();
+
+	// Checking for valid inputs
+  if (argc < 3) {
+    fprintf(stderr, "ERROR: no port provided");
+    exit(1);
+  }
+
+  /* Split and buffer the shit */
+  strcpy(f_name, argv[2]);
+  bufferFile();
+
+	// Creating the Internet domain socket 
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    fprintf(stderr, "ERROR opening socket");
+    exit(1);
+  }
+	
+  // zeroing the address structures 	
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  bzero((char *) &cli_addr, sizeof(cli_addr));	 
+   
+  portno = atoi(argv[1]);
+     
+	// Set server address values and bind the socket 
+	serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    fprintf(stderr, "ERROR on binding");
+  }
+
+  listen(sockfd,1);    
+	clilen = sizeof(cli_addr);
+	 
+	// accept connection and open this TCP socket
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if (newsockfd < 0) {
+    fprintf(stderr, "ERROR on accept");
+  }
+		
+  // reading Receiver's UDP port number
+  bzero(recv_udp_port,sizeof(int) + 1);
+	n = read(newsockfd,recv_udp_port, sizeof(recv_udp_port));
+  if (n < 0) {
+    fprintf(stderr, "ERROR reading from socket");
+    exit(1);
+  }
+  fprintf(stdout, "Here is Receiver's UDP port no. : %s\n",recv_udp_port);
+     
+	// sending file transfer info to server
+	char info[200];
+	sprintf(info, "%d %ld", SPLITS, splitSize); 
+	n = write(newsockfd, info, sizeof(info));
+  if (n < 0) {
+    fprintf(stderr, "ERROR writing to socket");
+    exit(1);
+  }
+  udpPort = atoi(recv_udp_port);
+  /* lets send the first chunk */
+  //exit(1);
+	/* TBD: Handle the timing in such a way that the 
+   * sleep is not required */
+  sleep(1);
+	
+	/* Inserting code to spawn multiple processes */
+	int pcounter = 0; /* Counter to spawn new process */
+	pid_t childId = 0;
+	int status = -1;
+	
+	/* Number of processes to be spawned = Number of splits */
+	while(pcounter < SPLITS)
+	{
+		childId = fork();
+		if(childId == 0)
+		{
+		  fprintf(stdout, "Child Process-%d started \n",pcounter+1);
+			/* assuming 1 process takes care of sending one split(chunk) 
+			* So chunk number argument is same as process counter arg 
+			*/
+			startUdp(pcounter,cli_addr);
+			fprintf(stdout, "Child Process-%d exiting \n",pcounter+1);
+		}
+		else if(childId < 0){
+			fprintf(stderr, "ERROR: Child Process Creation Failed");
+		}
+		else
+		{
+			fprintf(stdout, "Parent Process continuing \n");
+			pcounter++;
+		}
+	}
+	
+	/* Parent processes waits for child processes to terminate 
+	Signal Handling left !! 
+	*/
+	while (wait(&status) != -1);
+	
+	// startUdp(0, udpPort, cli_addr);
+  /* timing */
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Time taken: %f\n", time_spent);
+
+	// closing sockets
+  close(newsockfd);
+  close(sockfd);
+  return 0; 
+}
