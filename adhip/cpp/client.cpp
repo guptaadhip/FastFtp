49574952bf9abe270df3e026e406647e3431358f
Adhip Gupta <adhipgup@usc.edu>
2014-09-12 15:58:35 -0700

    cpp conversion

---
+++ adhip/cpp/client.cpp
@@ -0,0 +1,158 @@
+/* This is the TCP client socket */
+#include<stdlib.h>
+#include <iostream>
+#include <cstring>      // Needed for memset
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
+#include <cstdlib>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <cmath>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cctype>
+
+#define TCP_PORT_NO 7007
+#define UDP_PORT_NO 9000
+#define SPLITS 4
+#define DGRAM_SIZE 1450
+
+using namespace std;
+
+#define PATH "data.bin"
+
+int threadCounter[SPLITS];
+long int fileSize = 0;
+char *file;
+/* Server address */
+struct sockaddr_in tcpServerAddr;
+uint32_t splitSize = 0;
+
+/* Send file as UDP */
+void sendUdp(int idx) {
+  char msg[DGRAM_SIZE + 8];
+  struct sockaddr_in udpServerAddress;
+  int udpSocket;
+  socklen_t serverAddrLen;
+  long int sendPtr = 0;
+  long int sendSize = DGRAM_SIZE;
+  long int exactStart = idx * splitSize;
+  int rc = 0;
+  long int seqNum = 0;
+  long int sendSizeN = 0;
+  
+  udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (udpSocket < 0) {
+    fprintf(stderr, "Error: Creating Socket\n");
+    exit(1);
+  }
+
+  /* lets copy the address */
+  udpServerAddress = tcpServerAddr;
+  udpServerAddress.sin_port = htons(UDP_PORT_NO + idx);
+  
+  serverAddrLen = sizeof(udpServerAddress);
+  while (sendPtr < splitSize) {
+    if ((splitSize - sendPtr) < DGRAM_SIZE) {
+      sendSize = splitSize - sendPtr;
+    } else {
+      sendSize = DGRAM_SIZE;
+    }
+    /* fill the structure */
+    seqNum = htonl(sendPtr);
+    memcpy(msg, &seqNum, sizeof(seqNum));
+    sendSizeN = htonl(sendSize);
+    memcpy((msg+4), &sendSizeN, sizeof(sendSizeN));
+    memcpy((msg + 8), (file + exactStart + sendPtr), sendSize);
+    rc = sendto(udpSocket, &msg, sizeof(msg), 0,
+              (struct sockaddr *)&udpServerAddress, serverAddrLen);
+    if (rc < 0) {
+      fprintf(stderr, "Error: Sending Data\n");
+      exit(1);
+    }
+    sendPtr += sendSize;
+  }
+}
+
+/* send UDP file */
+void *udp(void *argc) {
+  int idx = *((int *) argc);
+  sendUdp(idx);
+  return 0;
+}
+
+void fileBuffer() {
+  int fd;
+  struct stat stats;
+  
+  if ((fd = open(PATH, O_RDONLY)) < 0) {
+    cout << "Error Opening the file" << endl;
+    exit(0);
+  }
+  if (fstat (fd, &stats) < 0) {
+    printf("Error in getting file size\n");
+    exit(0);
+  }
+  fileSize = stats.st_size;
+  if ((file = (char *) mmap(0, fileSize, PROT_READ, MAP_PRIVATE, fd, 0)) == (void*) -1) {
+    printf("Error caching the file\n");
+    exit(0);
+  }
+  splitSize = (fileSize / SPLITS) + 1;
+}
+
+/* main */
+int main(int argc, char *argv[]) {
+  int rc, i;
+  int tcpSocket;
+  struct hostent *serverInfo;
+  pthread_t thread[SPLITS];
+
+  /* buffer the file */
+  fileBuffer();
+  
+  /* get server details */
+  serverInfo = gethostbyname(argv[1]);
+  if (serverInfo == NULL) {
+    printf("Failed to get the server for the hostname\n");
+  }
+
+  /* Creating the Internet domain socket */
+  tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (tcpSocket < 0) {
+    printf("Socket creation failed\n");
+  }
+
+  /* Set server address values */
+  tcpServerAddr.sin_family = AF_INET;
+  bcopy((char *) serverInfo->h_addr,(char *) &tcpServerAddr.sin_addr.s_addr,
+                                                      serverInfo->h_length);
+  tcpServerAddr.sin_port = htons(TCP_PORT_NO);
+  if (connect(tcpSocket, (struct sockaddr *) &tcpServerAddr,
+                                              sizeof(tcpServerAddr)) < 0) {
+    printf("Conneting to server failed\n");
+  }
+  
+  rc = send(tcpSocket, &fileSize, sizeof(fileSize), 0);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR writing to socket\n");
+    exit(1);
+  }
+  
+  for(i = 0; i < SPLITS; i++) {
+    threadCounter[i]=i;
+    pthread_create(&thread[i], NULL, udp, (void *) &threadCounter[i]);
+  }
+  
+  for(i = 0; i < SPLITS; i++) {
+    pthread_join(thread[i], NULL);
+  }
+  
+  //close(tcpSocket);
+  return 0; 
+}
---
+++ adhip/cpp/server.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <cstring>      // Needed for memset
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
+#include <cstdlib>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <cmath>
+
+#define TCP_PORT_NO 7007
+#define UDP_PORT_NO 9000
+#define SPLITS 4
+#define DGRAM_SIZE 1450
+
+
+using namespace std;
+
+long int fileSize = 0;
+long int splitSize = 0;
+int threadCounter[SPLITS];
+char *file;
+
+/* NACK */
+struct missedDataNack {
+  int idx;
+  long int missedSeq;
+} missedData[65535];
+
+int missedDataPtr = 0;
+long int totalPackets[SPLITS];
+long int numPackets = 0;
+
+/* Receive UDP */
+void receiveUdp(int idx) {
+  long int seqNum = 0;
+  char buffer[DGRAM_SIZE + 8];
+  struct sockaddr_in clientAddr, serverAddr;
+  socklen_t clientAddrLen;
+  int udpSocket, rc;
+  long int recvSize = 0;
+  int yes = 1;
+  long int exactLocation = idx * splitSize;
+  long int recvDataSize = 0;
+  /* NACK */
+  long int expectedSeqNum = 0;
+
+  /* lets do some time out */
+  struct timeval tv;
+  tv.tv_sec = 0;
+  tv.tv_usec = 100000;
+  
+  udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+  if (udpSocket < 0) {
+    fprintf(stderr, "Error: Creating Socket\n");
+    exit(1);
+  }
+  
+  if (setsockopt(udpSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+    fprintf(stderr, "Error: Setting Socket Options\n");
+    exit(1);
+  }
+
+  /*if (setsockopt(udpSocket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+    fprintf(stderr, "Error: Setting Socket Options\n");
+    exit(1);
+  }
+  long int n = 1024 * 9000; //experiment with it
+  if (setsockopt(udpSocket, SOL_SOCKET, SO_RCVBUFFORCE, &n, sizeof(n)) == -1) {
+    fprintf(stderr, "Error: Setting Socket Options\n");
+    exit(1);
+  }*/
+
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(UDP_PORT_NO + idx);
+  //fprintf(stdout, "Listening on Port: %d\n", ntohs(serverAddr.sin_port));
+  //fflush(stdout);
+  
+  if (bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+    fprintf(stderr, "Error: Binding to Socket %d\n", ntohs(serverAddr.sin_port));
+    exit(1);
+  }
+	clientAddrLen = sizeof(clientAddr);
+  
+
+  while (totalPackets[idx] != numPackets) {
+    bzero((char *) buffer, sizeof(buffer));
+    rc = recvfrom(udpSocket, buffer, sizeof(buffer), 0,
+                  (struct sockaddr *) &clientAddr, &clientAddrLen);
+    if (rc < 0) {
+      fprintf(stderr, "Error: Receiving Data\n");
+      fflush(stderr);
+      exit(1);
+    }
+    totalPackets[idx]++;
+    memcpy(&seqNum, buffer, sizeof(long int));
+    seqNum = ntohl(seqNum);
+    memcpy(&recvDataSize, (buffer + 4), sizeof(long int));
+    recvDataSize = ntohl(recvDataSize);
+    recvSize += recvDataSize;
+    //printf("Got seqNum: %ld, size: %d\n", seqNum, rc);
+    if (expectedSeqNum < seqNum) {
+      while (expectedSeqNum != seqNum) {
+        /* need to get locking */
+        missedData[missedDataPtr++].idx = idx;
+        missedData[missedDataPtr++].missedSeq = expectedSeqNum;
+        expectedSeqNum += recvDataSize;
+      }
+    } else if (expectedSeqNum > seqNum) {
+      //printf("Out of order seq: %ld\n", seqNum);
+      /* got a packet out of order need to handle */
+    }
+    expectedSeqNum += recvDataSize;
+    memcpy((file + exactLocation + seqNum), (buffer + 8), recvDataSize); 
+    //printf("Data Read: %d, Total Received Size: %ld SeqNum: %ld\n", rc, recvSize, seqNum);
+  }
+}
+
+/* Start the UDP Servers */
+void *udp(void *argc) {
+  int idx = *((int *) argc);
+  while (fileSize == 0);
+  /* sanity to do the file size Split */
+  splitSize = ceil((double)(fileSize / SPLITS));
+  numPackets = ceil((double)(splitSize / DGRAM_SIZE));
+  struct timeval t0,t1;
+  gettimeofday(&t0, 0);
+  receiveUdp(idx);
+  /* lets do the needful now */
+  gettimeofday(&t1, 0);
+  long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
+  printf("Time taken: %ld microseconds\n", elapsed);
+  //printf("Server Idx: %d\n", idx);
+  /* wait for the tcp command to be received */
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  long int tot = 0;
+  int rc, i;
+  int waitSocket, tcpSocket;
+  long int size;
+  struct sockaddr_in tcpAddr;
+  struct sockaddr_in tcpClientAddr;
+  socklen_t clientLen;
+  pthread_t thread[SPLITS];
+	
+	// Creating the Internet domain socket
+  waitSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (waitSocket < 0) {
+    fprintf(stderr, "ERROR opening socket\n");
+    exit(1);
+  }
+  
+	// Set server address values and bind the socket
+	tcpAddr.sin_family = AF_INET;
+  tcpAddr.sin_addr.s_addr = INADDR_ANY;
+  tcpAddr.sin_port = htons(TCP_PORT_NO);
+  if (bind(waitSocket, (struct sockaddr *) &tcpAddr, sizeof(tcpAddr)) < 0) {
+    fprintf(stderr, "ERROR on binding\n");
+  }
+  
+  listen(waitSocket,1);
+	clientLen = sizeof(tcpClientAddr);
+  /* Lets do the udp server also */
+  for(i = 0; i < SPLITS; i++) {
+    threadCounter[i]=i;
+    pthread_create(&thread[i], NULL, udp, (void *) &threadCounter[i]);
+  }
+  
+	// accept connection and open TCP socket
+  tcpSocket = accept(waitSocket, (struct sockaddr *) &tcpClientAddr, &clientLen);
+  if (tcpSocket < 0) {
+    fprintf(stderr, "ERROR on accept\n");
+  }
+
+  rc = recv(tcpSocket, &size, sizeof(size), 0);
+  if (rc < 0) {
+    fprintf(stderr, "ERROR reading from socket\n");
+    exit(1);
+  }
+  file = (char *) malloc(size + 1);
+  fileSize = size;
+  //printf("Got File Size: %ld\n", fileSize);
+  
+  splitSize = fileSize / SPLITS;
+  
+  for(i = 0; i < SPLITS; i++) {
+    pthread_join(thread[i], NULL);
+  }
+  for (i = 0; i < SPLITS; i++) {
+    tot += totalPackets[i];
+  }
+  printf("Packets Received:%ld\n", tot); 
+  printf("Expected Packets:%ld\n", numPackets * SPLITS); 
+  printf("Missed Packets:%d\n", missedDataPtr); 
+  return 0;
+}
